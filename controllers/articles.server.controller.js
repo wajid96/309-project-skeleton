28c3b51a6f4d70a3ed997eaf41140d6f40b6b68b
Abdul Wajid <wajid0194696@gmail.com>
2017-10-25 03:29:58 +0000

    Assignment9&10

--- controllers/articles.server.controller.js
+++ controllers/articles.server.controller.js
@@ -3,6 +3,50 @@ var Article = require('./../models/Article.js');
 var errorHandler = require('./errors.server.controller');
 var _ = require('lodash');
 
+exports.all = function(req, res){
+  Article.find(function(err, data) {
+    if (err) {
+      return res.status(400).send({
+
+  				message: errorHandler.getErrorMessage(err)
+  			});
+    } else {
+      console.log("api called");
+
+        res.render('./../public/views/article/all.ejs', {
+          user: req.user || null,
+          request: req,
+          articles: data
+        });
+    }
+  });
+  
+};
+
+exports.edit = function(req, res){
+  res.render('./../public/views/article/edit.ejs', {
+    user: req.user || null,
+    request: req
+  });
+};
+
+exports.view = function(req, res){
+  res.render('./../public/views/article/view.ejs', {
+    user: req.user || null,
+    request: req
+  });
+};
+
+exports.new = function(req, res){
+  res.render('./../public/views/article/create.ejs', {
+    user: req.user || null,
+    request: req
+  });
+};
+
+
+
+
 module.exports.list = function(req, res) {
   Article.find(function(err, data) {
     if (err) {
--- routes/article.server.routes.js
+++ routes/article.server.routes.js
@@ -3,6 +3,18 @@ module.exports = function(app){
  var articles = require('./../controllers/articles.server.controller.js');
  var users = require('./../controllers/users.server.controller.js');
 
+ app.route('/articles/new')
+    .get(articles.new);
+    
+ app.route('/articles/all')
+    .get(articles.all);
+    
+ app.route('/articles/:articleId')
+    .get(articles.view);
+    
+ app.route('/articles/edit/:articleId')
+    .get(articles.edit);
+ 
  app.route('/api/articles')
 	.get(articles.list)
 	.post(users.requiresLogin, articles.create);
